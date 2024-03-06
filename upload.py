f630ffb76e9d7fa3005067a41e154e65f6d6d51f
sergio valle hidalgo <ser.valle@alumnos.duoc.cl>
2024-03-06 13:12:12 -0300

    Test GH Pages

--- upload.py
+++ upload.py
@@ -1,1 +1,16 @@
-from os import system;
+from os import system, getcwd, chdir;
+
+#Preparando archivos
+commit = input("Ingrese el nombre del commit")
+webpage = f"{getcwd()}\\dist\\portafolio-web\\browser"
+
+system("git add .")
+system(f'git commit -m "{commit}"')
+system("ng build")
+
+system("git push -u AngularBlog master")
+
+chdir(webpage)
+system("git add .")
+system(f'git commit -m "{commit}"')
+system("git push -u AngularBlog gh-pages")
